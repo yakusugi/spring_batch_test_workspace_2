1bbca1e9f187af32d920e739d9c6d92cd857639e
yakusugi <yosukeito1987@gmail.com>
2023-12-29 16:40:43 +0900

    commit dec 29 2023

--- src/main/java/com/springbatch/tasklet/CurrencyExchangeApiTasklet.java
+++ src/main/java/com/springbatch/tasklet/CurrencyExchangeApiTasklet.java
@@ -19,56 +19,96 @@ import java.util.Collections;
 
 //... [Other imports and class declaration]
 
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.core.io.FileSystemResource;
+import com.springbatch.domain.UserSpending;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Collections;
+import java.util.Date;
+
 public class CurrencyExchangeApiTasklet implements Tasklet {
 
-	private final String apiKey = "GHSOg6VcH44yR9oKUQBm19JPhoGbq0mD";
+    private final OkHttpClient client = new OkHttpClient();
+    private final String apiKey = "GHSOg6VcH44yR9oKUQBm19JPhoGbq0mD";
 
-	@Override
-	public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-		String sourceCurrencyCode = (String) chunkContext.getStepContext().getJobParameters().get("sourceCurrencyCode");
-		String targetCurrencyCode = (String) chunkContext.getStepContext().getJobParameters().get("targetCurrencyCode");
+    @Override
+    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+        String sourceCurrencyCode = (String) chunkContext.getStepContext().getJobParameters().get("sourceCurrencyCode");
+        String targetCurrencyCode = (String) chunkContext.getStepContext().getJobParameters().get("targetCurrencyCode");
 
-		Double totalPrice = (Double) chunkContext.getStepContext().getStepExecution().getJobExecution()
-				.getExecutionContext().get("totalPrice");
+        // Setting up the reader for the CSV file
+        FlatFileItemReader<UserSpending> reader = new FlatFileItemReader<>();
+        reader.setResource(new FileSystemResource("src/main/resources/data/Product_Details_Output9.csv"));
+        reader.setLineMapper(getLineMapper());
+        reader.open(chunkContext.getStepContext().getStepExecution().getExecutionContext());
 
-		if (totalPrice == null) {
-			throw new IllegalStateException("Total price not set in the execution context.");
-		}
+        UserSpending userSpending;
+        while ((userSpending = reader.read()) != null) {
+            // Call the API for each record
+            double conversionRate = getConversionRate(userSpending.getSpendingDate(), sourceCurrencyCode, targetCurrencyCode);
+            double convertedPrice = userSpending.getPrice() * conversionRate;
 
-		// Ensure the currency codes are valid
-		if (sourceCurrencyCode == null || targetCurrencyCode == null) {
-			throw new IllegalStateException("Currency codes not set properly.");
-		}
+            System.out.println(String.format("Original: %f, Converted: %f", userSpending.getPrice(), convertedPrice));
+            // You can also add this data to a list or another data structure
+        }
 
-		System.out.println(String.format("source and target (%s to %s)", targetCurrencyCode, sourceCurrencyCode));
+        reader.close();
+        return RepeatStatus.FINISHED;
+    }
 
-		String apiUrl = String.format("https://api.apilayer.com/exchangerates_data/latest?symbols=%s&base=%s",
-				sourceCurrencyCode, targetCurrencyCode);
+    private double getConversionRate(Date date, String sourceCurrency, String targetCurrency) throws IOException {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String formattedDate = dateFormat.format(date);
 
-		HttpHeaders headers = new HttpHeaders();
-		headers.set("apikey", apiKey);
-		headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-		HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
+        String url = String.format("https://api.apilayer.com/exchangerates_data/%s?symbols=%s&base=%s", 
+                                  formattedDate, targetCurrency, sourceCurrency);
 
-		RestTemplate restTemplate = new RestTemplate();
-		ResponseEntity<String> response = restTemplate.exchange(apiUrl, HttpMethod.GET, entity, String.class);
+        Request request = new Request.Builder()
+            .url(url)
+            .addHeader("apikey", apiKey)
+            .build();
 
-		ObjectMapper objectMapper = new ObjectMapper();
-		JsonNode rootNode = objectMapper.readTree(response.getBody());
-		JsonNode ratesNode = rootNode.path("rates");
-		double conversionRate = ratesNode.path(sourceCurrencyCode).asDouble();
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
 
-		double convertedTotal = totalPrice * conversionRate;
+            // Handle the response to extract the conversion rate
+            return parseConversionRate(response.body().string(), targetCurrency);
+        }
+    }
 
-		System.out.println(String.format("Converted Total (%s to %s): %f", targetCurrencyCode, sourceCurrencyCode,
-				convertedTotal));
-		
-		chunkContext.getStepContext()
-    		.getStepExecution()
-    		.getJobExecution()
-    		.getExecutionContext()
-    		.put("convertedTotal", convertedTotal);
+    private double parseConversionRate(String responseBody, String targetCurrency) {
+        // Implement JSON parsing logic to extract the conversion rate from the response
+        // This is a placeholder logic. You need to replace it with actual parsing based on the response structure.
+        return 0; // Placeholder return value
+    }
 
-		return RepeatStatus.FINISHED;
-	}
+    private DefaultLineMapper<UserSpending> getLineMapper() {
+        DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
+        lineTokenizer.setDelimiter(",");
+        lineTokenizer.setNames(new String[]{"spendingId", "email", "spendingDate", "storeName", "productName", 
+                                            "productType", "vatRate", "price", "note", "currencyCode", "quantity"});
+
+        BeanWrapperFieldSetMapper<UserSpending> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
+        fieldSetMapper.setTargetType(UserSpending.class);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        fieldSetMapper.setCustomEditors(Collections.singletonMap(Date.class, new CustomDateEditor(dateFormat, false)));
+
+        DefaultLineMapper<UserSpending> lineMapper = new DefaultLineMapper<>();
+        lineMapper.setLineTokenizer(lineTokenizer);
+        lineMapper.setFieldSetMapper(fieldSetMapper);
+
+        return lineMapper;
+    }
 }
